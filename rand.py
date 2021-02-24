7827aa99389d8a067928f151fbf2425b5ba3324f
tashadanner <tashalouisedanner@gmail.com>
2021-02-23 16:57:44 -0800

    Added python random module practice code

---
+++ rand.py
@@ -0,0 +1,18 @@
+random_number = random.randrange(100)
+
+correct_guess = False
+
+while not correct_guess:
+    user_input = input("Guess a number between 0 and 100: ")
+    try:
+      number = int(user_input)
+      if number == random_number:
+        correct_guess = True
+      elif number > random_number:
+        print("You guessed too high!")
+      elif number < random_number:
+        print("You guessed too low!")
+    except:
+      print("That is not a number")
+
+print("You guessed the right number!")
